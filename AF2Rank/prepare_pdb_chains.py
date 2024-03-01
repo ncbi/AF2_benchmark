b480b5b8b3f874e771ef2886591ecaa5b9b85742
ethanchen1301 <84855099+ethanchen1301@users.noreply.github.com>
2024-03-01 12:47:40 -0500

    Delete AF2Rank directory

--- AF2Rank/prepare_pdb_chains.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-import os
-import re
-
-wkdir = "/data/Chen_SIP_2022/PostBac23/RankProject/all_folds2/inputs/"
-pdbdir = "/data/Chen_SIP_2022/PostBac23/RankProject/pdbs/"
-rosettadir = "/data/Chen_SIP_2022/PostBac23/RosettaTutorials/for_auto_generate/"
-fastamegafile = "/data/Chen_SIP_2022/PostBac23/RankProject/rcsb_pdb_20230308103107.fasta"
